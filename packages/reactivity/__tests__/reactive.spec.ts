2600cea5c8d6e3afccee1a60bec29c159f96c25d
Jiang Sheng <jiangshengdev@outlook.com>
2023-01-14 17:10:28 +0800

    test

--- packages/reactivity/__tests__/reactive.spec.ts
+++ packages/reactivity/__tests__/reactive.spec.ts
@@ -14,4 +14,44 @@ describe('reactivity/reactive', () => {
     // ownKeys
     expect(Object.keys(observed)).toEqual(['foo'])
   })
+
+  test('observed value should proxy mutations to original (Object)', () => {
+    const original: any = { foo: 1 }
+    const observed = reactive(original)
+    // set
+    observed.bar = 1
+    expect(observed.bar).toBe(1)
+    expect(original.bar).toBe(1)
+    // delete
+    delete observed.foo
+    expect('foo' in observed).toBe(false)
+    expect('foo' in original).toBe(false)
+  })
+
+  test('original value change should reflect in observed value (Object)', () => {
+    const original: any = { foo: 1 }
+    const observed = reactive(original)
+    // set
+    original.bar = 1
+    expect(original.bar).toBe(1)
+    expect(observed.bar).toBe(1)
+    // delete
+    delete original.foo
+    expect('foo' in original).toBe(false)
+    expect('foo' in observed).toBe(false)
+  })
+
+  test('observing already observed value should return same Proxy', () => {
+    const original = { foo: 1 }
+    const observed = reactive(original)
+    const observed2 = reactive(observed)
+    expect(observed2).toBe(observed)
+  })
+
+  test('observing the same value multiple times should return same Proxy', () => {
+    const original = { foo: 1 }
+    const observed = reactive(original)
+    const observed2 = reactive(original)
+    expect(observed2).toBe(observed)
+  })
 })
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -5,17 +5,40 @@ export const enum ReactiveFlags {
   IS_REACTIVE = '__v_isReactive'
 }
 
-export const reactiveMap = new WeakMap<Target, any>()
-
 export interface Target {
   [ReactiveFlags.IS_REACTIVE]?: boolean
 }
 
+export const reactiveMap = new WeakMap<Target, any>()
+
+/**
+ * Creates a reactive copy of the original object.
+ *
+ * The reactive conversion is "deep"—it affects all nested properties. In the
+ * ES2015 Proxy based implementation, the returned proxy is **not** equal to the
+ * original object. It is recommended to work exclusively with the reactive
+ * proxy and avoid relying on the original object.
+ *
+ * A reactive object also automatically unwraps refs contained in it, so you
+ * don't need to use `.value` when accessing and mutating their value:
+ *
+ * ```js
+ * const count = ref(0)
+ * const obj = reactive({
+ *   count
+ * })
+ *
+ * obj.count++
+ * obj.count // -> 1
+ * count.value // -> 1
+ * ```
+ */
 export function reactive(target: object) {
   if (!isObject(target)) {
     return target
   }
 
+  // target is already a Proxy, return it.
   if (target[ReactiveFlags.IS_REACTIVE]) {
     return target
   }
